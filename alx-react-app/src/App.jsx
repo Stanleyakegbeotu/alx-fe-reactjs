5d904f6de3082fef16c8b4a311c9b9c7ff0657c4
Stanley Akegbeotu <stanleyakegbeotu@gmail.com>
2026-01-28 14:11:49 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,15 @@
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   return (
     <div>
+      <Header />
       <WelcomeMessage />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
