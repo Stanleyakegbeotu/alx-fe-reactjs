e0d9a456762cc171fec32b103c7ab7c18f2adeb9
Stanley Akegbeotu <stanleyakegbeotu@gmail.com>
2026-01-28 14:17:41 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,15 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
       <Header />
-      <WelcomeMessage />
+      {/* Passing different data to the same component using props */}
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserProfile name="John" age="30" bio="Avid traveler and foodie" />
       <MainContent />
       <Footer />
     </div>
