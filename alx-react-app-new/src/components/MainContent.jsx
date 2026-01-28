8b597e434efed360e3e0795872d5e2c4233c8ec0
Stanley Akegbeotu <stanleyakegbeotu@gmail.com>
2026-01-28 20:16:12 +0100

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,8 +1,8 @@
 function MainContent() {
     return (
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
+      <main style={{ padding: '20px', lineHeight: '1.6', color: '#333' }}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
     );
 }
 export default MainContent;
