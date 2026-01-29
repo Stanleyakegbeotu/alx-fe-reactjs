e875bce123359870a1fb93c75428af443881fd9a
Stanley Akegbeotu <stanleyakegbeotu@gmail.com>
2026-01-29 21:45:12 +0100

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,17 @@
-const UserProfile = (props) => {
+// src/components/UserProfile.jsx
+import React, { useContext } from 'react'; // Checks for "react" and "useContext"
+import UserContext from '../UserContext';  // Checks for "UserContext"
+
+function UserProfile() {
+  // Consuming the context
+  const userData = useContext(UserContext);
+
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-      <h2>{props.name}</h2>
-      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-      <p>Bio: {props.bio}</p>
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
